e439c1b3a890bac3e6ce03e553db9cfe3d53caa8
Alexandre Roux <alex@tekartik.com>
2023-11-05 12:30:49 +0100

    example list all current branch

--- example/bin/list_recursive_git_path.dart
+++ example/bin/list_recursive_git_path.dart
@@ -1,7 +1,9 @@
 import 'package:tekartik_sc/git.dart';
 
 Future<void> main() async {
-  await recursiveGitRun(['.'], action: (path) {
+  await recursiveGitRun(['.'], action: (path) async {
+    var currentBranch = await GitPath(path).getCurrentBranch();
     print('path: $path');
+    print('  branch: $currentBranch');
   });
 }
